017691215da6d4469c8937a7f943460918c648c7
devfernandodev <luisfernandoreturn@gmail.com>
2025-02-17 20:47:58 -0300

    Aula 02 sobre tipos primitivos

---
+++ src/fernando/qa/comjava/introducao/Aula01OlaMundo.java
@@ -0,0 +1,9 @@
+package fernando.qa.comjava.introducao;
+
+public class Aula01OlaMundo {
+    public static void main (String[] args){
+        System.out.println("Olá Mundo estou aprendendo java");
+
+
+    }
+}
---
+++ src/fernando/qa/comjava/introducao/Aula02TiposPrimitivos.java
@@ -0,0 +1,18 @@
+package fernando.qa.comjava.introducao;
+
+public class Aula02TiposPrimitivos {
+    public static void main(String[] args) {
+        // int, double,float,char, boolean, byte, short, long
+        int idade = 27;
+        long numeroGrande = 100000;
+        double salarioDouble = 4000;
+        float salarioFloat = 8000;
+        byte idadeByte = 27;
+        short idadeShort = 27;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char  caractere = 65;
+
+        System.out.println("my age is: "+ idade);
+    }
+}
