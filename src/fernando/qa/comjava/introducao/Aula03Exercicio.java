f6ffe96efaabaf11f31d35fe93f91bfb2b75cb96
QAbyFernando <luisfeferrari.ti@gmail.com>
2025-02-17 21:57:10 -0300

    exercicio para fixar os tipos primitivos e nao primitivos

---
+++ src/fernando/qa/comjava/introducao/Aula03Exercicio.java
@@ -0,0 +1,12 @@
+package fernando.qa.comjava.introducao;
+//Eu <nome>, morando no endereço <endereço>, confirmo que recebi o salário de <salario>, na data <data>.
+public class Aula03Exercicio {
+    public static void main(String[] args) {
+        String nome = "Fernando";
+        String dataRecebimento = "17/05/2025";
+        String endereco = "Rua goku da silva";
+        double salario = 8500;
+
+        System.out.println("Eu "+ nome +" morando na " + endereco + " comfirmo que recebi o salario de: " + salario + " na data de: "+ dataRecebimento);
+    }
+}
